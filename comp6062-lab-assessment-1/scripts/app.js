10fcd78765d294e95af40d9b7399f50efeaf57ab
kishore-360 <kishoreveerappan2@gmail.com>
2025-06-12 19:45:15 -0400

    Lab Assessment1

---
+++ comp6062-lab-assessment-1/scripts/app.js
@@ -0,0 +1,8 @@
+const studentName = 'Kishore Veerappan';
+const studentNumber = 1219133;
+const result = `${studentName} - ${studentNumber}`;
+console.log(result);
+let headingprimaryContent = document.querySelector("h1");
+headingprimaryContent.innerHTML = "Kishore Veerappan - 1219133";
+headingprimaryContent.classList.add("headingprimary");
+
